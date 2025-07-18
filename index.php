517aeedb1895af300c58e7bf70ba6c324c9a1d95
MattiaToscano <mattia1997ct@gmail.com>
2025-07-18 13:33:12 +0200

    [UPD] Creata struttura base del sito e aggiunto README

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Password</title>
 </head>
 <body>
     
+<form>
+    <h1> Grande generatore di password</h1>
+    <br>
+
+  <div class="mb-3 container-fluid">
+    <label for="exampleInputPassword1" class="form-label">Genera Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Genera</button>
+</form>
+
+
 </body>
 </html>
